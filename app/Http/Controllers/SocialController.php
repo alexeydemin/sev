ac4e9b65f296ba1bb1591677b4d1eaa6d0ba13bd
Alexey Demin <deminn@gmail.com>
2019-03-20 21:15:02 +0000

    logging added

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -29,13 +29,16 @@ class SocialController extends Controller
         file_put_contents(storage_path('mylog.log'), print_r($_GET, 1) . "\n", FILE_APPEND);
         file_put_contents(storage_path('mylog.log'), "== END GET ==\n", FILE_APPEND);
 
-        file_put_contents(storage_path('mylog.log'), "== POST ==\n", FILE_APPEND);
+        file_put_contents(storage_path('mylog.log'), date('d.m.y H:i:s') . "[]== POST ==\n", FILE_APPEND);
         file_put_contents(storage_path('mylog.log'), print_r($_POST, 1) . "\n", FILE_APPEND);
+        file_put_contents(storage_path('mylog.log'), file_get_contents('php://input') . "\n", FILE_APPEND);
         file_put_contents(storage_path('mylog.log'), "== END POST ==\n", FILE_APPEND);
 
 
         if(isset($_GET['hub_challenge'])) {
             echo $_GET['hub_challenge'];
         }
+
+        //return view('webhook');
     }
 }
